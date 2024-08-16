f26472446b003ff81d08ac9bbc75cf5512defe5d
0xreself <lars.m.schneider2002@gmail.com>
2024-08-16 23:42:37 +0200

    + mouse cursor grid

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,12 @@ public class CameraController : MonoBehaviour
 {
     private Vector3 lastPos = Vector3.zero;
 
+    [SerializeField]
+    private GameObject gridCursor;
+
+    [SerializeField]
+    private float gridCursorSpeed = 5;
+
     void Start() {
         if (this.GetComponent<Camera>() == null) {
             Debug.LogError("CameraController not on Gameobject with Camera");
@@ -12,10 +18,24 @@ public class CameraController : MonoBehaviour
 
     void Update() {
         HandleMouseInput();
+        HandleGridCursor();
     }
 
-    private Vector3 GetMousePositionInWorldSpace() {
-        return this.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+    private Vector3  GetMousePositionInWorldSpace() {
+        Vector3 worldPosition = this.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+
+        return new Vector3(worldPosition.x, worldPosition.y);
+    }
+
+    private void HandleGridCursor() {
+        Vector3 currentGridPos = GetCenterGridPosition(GetGridPosition());
+        Vector3 difference = currentGridPos - new Vector3(gridCursor.transform.position.x, gridCursor.transform.position.y);
+
+        
+        /*if (difference.magnitude > 1) {
+            difference = Vector3.Normalize(difference);
+        }*/
+        gridCursor.transform.position += gridCursorSpeed * Time.deltaTime * difference;
     }
 
     private void HandleMouseInput() {
@@ -27,5 +47,13 @@ public class CameraController : MonoBehaviour
         }
 
         lastPos = GetMousePositionInWorldSpace();   
+    }
+
+    public Vector3 GetGridPosition() {
+        return Vector3Int.FloorToInt(GetMousePositionInWorldSpace());
+    }
+
+    public static Vector3 GetCenterGridPosition(Vector3 gridPosition) {
+        return gridPosition += new Vector3(0.5f, 0.5f);
     }
 }
