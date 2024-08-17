5911667afc6b6d027f18bfb24e104ae72fc3cb9e
Clock-work <clock-work@gmx.de>
2024-08-17 13:45:44 +0200

    - for now compiler errrors are fixed

--- Assets/Scripts/Entities/Items/Item.cs
+++ Assets/Scripts/Entities/Items/Item.cs
@@ -11,13 +11,52 @@ public class Item : MonoBehaviour
 	private int processingTime = 0;
 
 	// the remaining time it takes to process the item within the current item holder 
-	private int remainingProcessingTime = 0; 
+	private int remainingProcessingTime = 0;
+
+	// in which holder the item is currently in
+	private ItemHolder holder;
 
 
-	// resets the processing time of this item (when it arrives at a new item holder) 
-	public void reset()
+	// resets the processing time of this item (when it arrives at a new item holder). 
+
+	// this can also 
+	public virtual void move(ItemHolder holder)
 	{
 		remainingProcessingTime = processingTime;
+		this.holder = holder;
+	}
+
+	// called by the holder to proccess the item
+	public virtual void process(int speed)
+	{
+		if(holder!=null)
+		{
+			if(remainingProcessingTime > 0)
+			{
+				remainingProcessingTime -= speed;
+			}
+		}
+		else
+		{
+			Debug.LogError("Item process had no holder!");
+		}
+	}
+
+	// returns if the item can be processed by the holder. (sub class can add custom behaviour, but must return super.canBeProcessed) 
+	public virtual bool canBeProcessed()
+	{
+		return holder != null && remainingProcessingTime > 0;
+	}
+
+	// returns if the item was processed by the holder. (sub class can add custom behaviour, but must return super.isProcessed) 
+	public virtual bool isProcessed()
+	{
+		return holder != null && remainingProcessingTime <= 0;
+	}
+
+	public void delete()
+	{
+		Destroy(this.gameObject);
 	}
 
 
