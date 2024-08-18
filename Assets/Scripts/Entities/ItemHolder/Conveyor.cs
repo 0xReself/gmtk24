778686cb48cc516cc002f47fb719d4c186b0f826
Clock-work <clock-work@gmx.de>
2024-08-18 10:05:35 +0200

    - restricted splitter so that it can not add items to side of conveyor belts

--- Assets/Scripts/Entities/ItemHolder/Conveyor.cs
+++ Assets/Scripts/Entities/ItemHolder/Conveyor.cs
@@ -9,4 +9,14 @@ using static UnityEditor.Progress;
 public class Conveyor : ItemHolder
 {
 
+	public override bool canAcceptItem(Item item, int connectionSidePosition, ItemHolder otherHolder)
+	{
+		ConnectionSide side = getConnectionSides()[connectionSidePosition];
+		if(otherHolder is Splitter && side == ConnectionSide.sideInput)
+		{
+			return false;
+		}
+		return base.canAcceptItem(item, connectionSidePosition, otherHolder);
+	}
+
 }
